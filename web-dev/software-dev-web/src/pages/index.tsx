69bf9280d982e89ecf9ece95d7fa2192c012ac5e
Davi Alcântara <dxvialcantara@gmail.com>
2022-03-25 20:41:23 -0300

    web-dev 22/03

--- web-dev/software-dev-web/src/pages/index.tsx
+++ web-dev/software-dev-web/src/pages/index.tsx
@@ -2,9 +2,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
 
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>Hello world</h1>
 		</div>
 	);
--- web-dev/software-dev-web/src/pages/posts/index.jsx
+++ web-dev/software-dev-web/src/pages/posts/index.jsx
@@ -2,21 +2,14 @@ import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 
-import PostCard from './PostCard';
 import Link from 'next/link';
+import PostCard from '../../components/PostCard';
 
 export default function Posts({ posts }) {
 	return (
 		<div className={styles.container}>
 			{posts.map((post) => (
-				<div className={styles.card} key={post.id}>
-					<Link href={`/posts/${post.id}`}>
-						<a>
-							<h2>{post.name}</h2>
-							<p>{post.body}</p>
-						</a>
-					</Link>
-				</div>
+				<PostCard post={post} key={post.id} />
 			))}
 		</div>
 	);
